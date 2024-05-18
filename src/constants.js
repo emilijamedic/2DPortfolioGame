cd137bddaac8be4113513a96db5617d320a2c750
emilijamedic <gargamel998@gmail.com>
2024-05-18 05:34:59 +0200

    Add files via upload

---
+++ src/constants.js
@@ -0,0 +1,17 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `Regardless of the language, I just like programming.
+    Here is my <a href="https://github.com/emilijamedic" target="_blank">Github</a>!`,
+  "sde-degree": `This is my SDE degree. I hung it on the wall because I'm proud of it. It was a very fun degree to get and i met so many wonderful people aloneg the way! 
+  I think it gave me a good foundation.`,
+  "sofa-table": `That's my sofa. I like to relax here and watch YouTube.`,
+  tv: `That's my TV. I know, this is a very inspiring message.`,
+  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+  resume: `This is my desk and on it is my resume. <a href="https://github.com/emilijamedic/resume" target="_blank">Check it out?</a>
+  Contact me at emilija.medic.19@singimail.rs if you have any interesting job opportunities!`,
+  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas.`,
+  library: `There are a lot of programming books on my shelves.`,
+  exit: `If you want to exit my portfolio, just close the tab.`,
+};
